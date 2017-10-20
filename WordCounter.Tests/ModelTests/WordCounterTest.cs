6c049baf52f7c57a49084eafdbd92bb79b787b61
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-10-20 15:15:55 -0700

    it updates the startup ,program,and csproj files WordCounter and WordCounter.Tests folder.

---
+++ WordCounter.Tests/ModelTests/WordCounterTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using WordCounter;
+
+namespace WordCounter.Tests
+{
+
+
+
+
+
+    
+}
---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+
+
+namespace WordCounter
+{
+    
+
+
+
+
+
+
+
+
+
+
+}
